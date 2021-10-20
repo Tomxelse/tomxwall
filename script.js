691d2757e88de6e06508e75bf8fd6587f5485d50
Tomasz Czerwinski <92301036+Tomxelse@users.noreply.github.com>
2021-10-20 14:46:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-function time() {
-    var d = new Date();
-    var h = (d.getHours() + d.getMinutes()/60) / 12 * 360;
-    var m = (d.getMinutes() + d.getSeconds()/60) / 60 * 360;
-    var s = (d.getSeconds() + d.getMilliseconds()/1000) /60 * 360;
-    $('#watch .hands .h').css('transform', 'rotate('+h+'deg)');
-    $('#watch .hands .m').css('transform', 'rotate('+m+'deg)');
-    $('#watch .hands .s').css('transform', 'rotate('+s+'deg)');
-}
-
-time();
-setInterval(time, 10);
+      // JS is  used to get the current time (modified from https://codepen.io/kowlor/pen/bdRzML) and trigger chronograph animations.
+      
+      function setTime() {
+        var d = new Date;
+      
+        function ct(v) {
+          return v * 360 / 60;
+        };
+      
+        function ctm(v) {
+          return v * 360 / 60 + 6 * ct(d.getSeconds()) / 360; // 1min = 6 deg
+        };
+      
+        function cth(v) {
+          return v * 360 / 12 + 30 * ct(d.getMinutes()) / 360; // 1h = 30 deg
+        };
+      
+        document.getElementsByClassName('sec')[0].style.transform = 'rotateZ(' + ct(d.getSeconds()) + 'deg)';
+        document.getElementsByClassName('min')[0].style.transform = 'rotateZ(' + ctm(d.getMinutes()) + 'deg)';
+        document.getElementsByClassName('hr')[0].style.transform = 'rotateZ(' + cth(d.getHours()) + 'deg)';
+      }
+      
+      document.getElementById('start-stop').onclick = function(e) {
+        e.preventDefault();
+        var elem = document.getElementsByClassName('stopwatch');
+        var state = 'running' == elem[0].style.animationPlayState ? 'paused' : 'running';
+        for (var i = 0; i < elem.length; i++) {
+      	  elem[i].style.animationPlayState = state;
+      	  if (-1 == elem[i].className.indexOf('animate')) {
+      		  elem[i].className = elem[i].className + ' animate';
+      	  }
+        }
+      };
+      
+      document.getElementById('reset').onclick = function(e) {
+        e.preventDefault();
+        var newone;
+        var elem = document.getElementsByClassName('stopwatch');
+        for (var i = 0; i < elem.length; i++) {
+      	  newone = elem[i].cloneNode(true);
+      	  elem[i].parentNode.replaceChild(newone, elem[i]);
+        }
+      };
+      
+      setTime();
